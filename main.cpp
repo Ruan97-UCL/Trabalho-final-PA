5abaa50e606b5b8ef5fec083446c81d5c54d851e
ruanpardinho <82624108+Ruan97-UCL@users.noreply.github.com>
2021-07-02 23:18:06 +0000

    Testes finais

--- main.cpp
+++ main.cpp
@@ -1,197 +1,160 @@
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <cctype>
+#include <iostream>
 
 using namespace std;
 
-struct tComplexos{
-  float real;
-  float imag;
+struct tComplexos {
+	float real;
+	float imag;
 };
 
-class No
-{
-  public:
-  tComplexos valor;
-  No* proximo;
+class No {
+public:
+	tComplexos valor;
+	No *proximo;
 
-  No(tComplexos elemento)
-  {
-    this->valor=elemento;
-    this->proximo=NULL;
-  }
+	No(tComplexos elemento) {
+		this->valor = elemento;
+		this->proximo = NULL;
+	}
 
-  void definirProximo(No* end)
-  {
-    proximo=end;
-  }
+	void definirProximo(No *end) {
+		proximo = end;
+	}
 
-  tComplexos obterValor()
-  {
-    return valor;
-  }
+	tComplexos obterValor() {
+		return valor;
+	}
 
-  float obterReal()
-  {
-    return valor.real;
-  }
+	float obterReal() {
+		return valor.real;
+	}
 
-  float obterImag()
-  {
-    return valor.imag;
-  }
+	float obterImag() {
+		return valor.imag;
+	}
 
-  No* obterProximo()
-  {
-    return proximo;
-  }
+	No *obterProximo() {
+		return proximo;
+	}
 };
 
-class Lista
-{
-  public:
-  No* inicio;
-  No* fim;
+class Lista {
+public:
+	No *inicio;
+	No *fim;
 
-  Lista()
-  {
-    inicio = NULL;
-    fim=NULL;
-  }
+	Lista() {
+		inicio = NULL;
+		fim = NULL;
+	}
 
-  Lista(tComplexos n)
-  {
-    inicio = new No(n);
-    fim = inicio;
-  }
+	Lista(tComplexos n) {
+		inicio = new No(n);
+		fim = inicio;
+	}
 
-  virtual ~Lista()
-  {
-    delete inicio;
-  }
+	virtual ~Lista() {
+		delete inicio;
+	}
 
-  bool seVazia()
-  {
-    return (inicio == NULL);
-  }
+	bool seVazia() {
+		return (inicio == NULL);
+	}
 
-  void imprimirElementos()
-  {
-    cout<<"ELEMENTOS DA LISTA"<<endl;
-    No* i=inicio;
-    if(seVazia())
-    {
-      cout<<"Lista sem elementos!"<<endl;
-    }
-    else
-    {
-      while(i)
-      {   
-        
-        cout << i->obterReal() << " " << i->obterImag() <<endl;
-        i = i->obterProximo();
-      }
-      cout<<endl;
-    }
-  }
+	void imprimirElementos() {
+		cout << "ELEMENTOS DA LISTA" << endl;
+		No *i = inicio;
+		if (seVazia()) {
+			cout << "Lista sem elementos!" << endl;
+		} else {
+			while (i) {
+				cout << i->obterReal() << " " << i->obterImag() << endl;
+				i = i->obterProximo();
+			}
+			cout << endl;
+		}
+	}
 
-  void insereNoFim(tComplexos valor)
-  {
-    No* novo = new No(valor);
+	void insereNoFim(tComplexos valor) {
+		No *novo = new No(valor);
 
-    if(seVazia())
-    {
-      inicio = novo;
-      fim = novo;
-    }
-    else
-    {
-      fim -> definirProximo(novo);
-      fim = novo;
-    }
-  }
+		if (seVazia()) {
+			inicio = novo;
+			fim = novo;
+		} else {
+			fim->definirProximo(novo);
+			fim = novo;
+		}
+	}
 
-  int tamanhoLista()
-  { 
-    if(seVazia())
-    {
-      return 0;
-    }
-    No* i = inicio;
-    int tamanho = 0;
+	int tamanhoLista() {
+		if (seVazia()) {
+			return 0;
+		}
+		No *i = inicio;
+		int tamanho = 0;
 
-    do
-    {
-      i = i->obterProximo();
-      tamanho++;
-    }while(i);
+		do {
+			i = i->obterProximo();
+			tamanho++;
+		} while (i);
 
-    return tamanho;
-  }
+		return tamanho;
+	}
 
-void botaArquivo(tComplexos n){
-  No *i = inicio;
-  ofstream poe;
-  poe.open("COMPLEXOS.txt", ios::out);
-        while(i)
-      {         
-        poe << i->obterReal() << " " << i->obterImag() << endl;
-        i = i->obterProximo();
-      }
-    poe.close();
- }
-
-void leArquivo(){
-    ifstream pega;
-    ifstream PORRA;
-    tComplexos valor;
-    No *i = inicio;
-    string str;
-    pega.open("COMPLEXOS.txt", ios::in);
-
-      while(i){
-        while(pega >> valor.real >> valor.imag){
-              insereNoFim(valor);
-         }                         i=i->obterProximo();  
-       }
-     pega.close();
- }
+	void botaArquivo(tComplexos n) {
+		No *i = inicio;
+		ofstream poe;
+		poe.open("COMPLEXOS.txt", ios::out);
+		while (i) {
+			poe << i->obterReal() << " " << i->obterImag() << endl;
+			i = i->obterProximo();
+		}
+		poe.close();
+	}
 };
+	void leArquivo(Lista* lista) {
+		ifstream pega;
+		tComplexos valor;
+		No *i = lista->inicio;
+		pega.open("COMPLEXOS.txt", ios::in); 
+		//while (i) {
+			while (pega >> valor.real >> valor.imag) {
+				lista->insereNoFim(valor);
+			}
+			//i = i->obterProximo();
+		//}
+		pega.close();
+	 }
+
+
+int main() {
+	tComplexos n;
+	int s, quant;
+	Lista *lista = new Lista;
   
-int main(){   
 
-    tComplexos n; 
-    int s, quant;
-    Lista* lista = new Lista;    
+	do {
+		cout << "\nPressione 0 para gravar, 1 para ler, -1 para sair\n";
+		cin >> s;
+		if (s == 0) {
+			cout << "\nInforme a quantidade de complexos: \n";
+			cin >> quant;
+			for (int i = 0; i < quant; i++) {
+				cout << "\nInforme o complexo\n";
+				cin >> n.real;
+				cin >> n.imag;
+				lista->insereNoFim(n);
+			}
+			lista->botaArquivo(n);
+		}
 
-do
-  {
-    cout << "\nPressione 0 para gravar, 1 para ler, -1 para sair\n";
-    cin >> s;
-        if(s==0)
-        {
-            cout << "\nInforme a quantidade de complexos: \n";
-            cin >> quant; 
-            for (int i = 0; i<quant; i++){
-              cout << "\nInforme o complexo\n";
-              cin >> n.real;
-              cin >> n.imag; 
-              lista->insereNoFim(n);
-             }
-              lista->botaArquivo(n);
-        }
-        
-           else if(s==1)
-           {  
-              //lista->~Lista();
-              if(!lista->seVazia()){
-              *lista = Lista(n);
-              lista->leArquivo();
-              lista->imprimirElementos();
-              }
-           }        
-           
-      //lista->imprimirElementos();
-   } while (((s!=1)||(s!=2)) && (s!=-1));
+		else if (s == 1) {
+		  lista = new Lista;
+			leArquivo(lista);
+			lista->imprimirElementos();
+		}
+		// lista->imprimirElementos();
+	} while (((s != 1) || (s != 2)) && (s != -1));
 }
